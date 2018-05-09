6da513351ecad21a988c9fc5bd692fe3b93d104a
nguyenlyminhman@gmail.com <nguyenlyminhman@gmail.com>
2018-05-09 19:04:23 +0700

    Adding db-connection.js file and some ejs template file

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const app = express();
 const config = require('config');
+const mongodb = require('mongodb');
+const bodyParser = require('body-parser');
 let controller = require(__dirname + "/app/controllers");
 
-app.set("views", __dirname+"/app/views");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('trust proxy', 1);
+
+app.set("views", __dirname + "/app/views");
 app.set("view engine", "ejs");
 app.use("static", express.static(__dirname + "public"));
 
 app.use(controller);
 
-let host = config.get("server.host");
-let port = config.get("server.port");
+let port = process.env.PORT || config.get("server.port");
 
 app.listen(port, () => {
     console.log('CRUD mongodb app listening on port', port);
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
 
 router.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        title: "Crud with mongodb"
+    });
+});
+
+router.get('/user/add-new', (req, res) => {
+    res.render('add', {
+        title: "Add new user"
+    });
+});
+
+router.get('/user/view-list', (req, res) => {
+    res.render('list', {
+        title: "List the user"
+    });
 });
 
 module.exports = router;
